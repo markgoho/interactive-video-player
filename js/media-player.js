f8395a742ec68652eb5492839d7eb55a0bf9b20c
Mark Goho <markgoho@gmail.com>
2016-09-24 22:20:15 -0400

    Getting video player to play

---
+++ js/media-player.js
@@ -0,0 +1,23 @@
+var video = document.getElementById('#video-player');
+
+$playPauseButton = $('#play-pause-button');
+
+// $playPauseButton.click(function () {
+// 	if (video.paused) {
+// 		video.play();
+// 	} else {
+// 		video.pause();
+// 	}
+	
+// });
+
+$playPauseButton.click(function () {
+  if (video.paused) {
+    video.play();
+    $playPauseButton.css('background', 'url(icons/pause-icon.png) center center no-repeat');
+  } else {
+    video.pause();
+    $playPauseButton.css('background', 'url(icons/play-icon.png) center center no-repeat');
+  }
+});
+
